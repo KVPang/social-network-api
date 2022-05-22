6e9a2494b2ee4d45424106cc3b0079aafbe52d5a
Kristina Pang <kvypang@gmail.com>
2022-05-22 15:56:40 -0700

    user and thought models

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Thought text is Required',
+            minlength: 1,
+            maxlength: 280
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal 
+        },
+
+        username: {
+            type: String,
+            required: 'Username is Required'
+        },
+
+        reactions: [ ReactionSchema ]
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,46 @@
 const { Schema, model } = require('mongoose');
 
-const User = model('User', UserSchema);
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: 'Username is Required',
+            unique: true
+        },
 
+        email: {
+            type: String,
+            unique: true,
+            match: [/.+@.+\..+/],
+            required: 'Passowrd is Required'
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+
+const User = model('User', UserSchema);
 module.exports = User;
