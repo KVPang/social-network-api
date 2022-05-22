02c9ddd1de80bc867f63668c27e7c6cc9546f1e8
Kristina Pang <kvypang@gmail.com>
2022-05-22 15:49:42 -0700

    add route files

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
