ab51ee5b780fed28dfd34a763db86589775956b1
Kristina Pang <kvypang@gmail.com>
2022-05-24 17:09:58 -0700

    changed route in thoughts from put to post

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {addReaction, createThought, updateThought, deleteThought, getAllThoughts, getThoughtById, deleteReaction} = require('../../controllers/thought-controller')
 router.route("/").post(createThought).get(getAllThoughts)
 router.route("/:thoughtId").delete(deleteThought).put(updateThought).get(getThoughtById)
-router.route("/reactions/:thoughtId").put(addReaction)
+router.route("/reactions/:thoughtId").post(addReaction)
 router.route("/reactions/:thoughtId/:reactionId").delete(deleteReaction)
 
 module.exports = router;
